a6f5a21f625a9966d909bf6453befab6bd0c0d50
Toya-Vargas <toyavaroa@gmail.com>
2021-11-26 17:42:06 -0500

    DB Access and port 8081 for to expose

--- src/main/java/com/misiontic/account_ms/controllers/AccountController.java
+++ src/main/java/com/misiontic/account_ms/controllers/AccountController.java
@@ -3,6 +3,7 @@ import com.misiontic.account_ms.exceptions.AccountNotFoundException;
 import com.misiontic.account_ms.models.Account;
 import com.misiontic.account_ms.repositories.AccountRepository;
 import org.springframework.web.bind.annotation.*;
+
 @RestController
 public class AccountController {
     private final AccountRepository accountRepository;
